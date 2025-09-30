64a83b7187d8f777408b08f8305af6fcfb570347
Sergio Nivin <snivin@sparxworks.com>
2025-09-30 15:43:19 -0500

    Test: probando Husky y ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,15 @@
-// eslint.config.js
 import js from '@eslint/js';
 import astro from 'eslint-plugin-astro';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
-  // 1. Reglas recomendadas de ESLint
   js.configs.recommended,
-
-  // 2. Reglas recomendadas para Astro
   ...astro.configs.recommended,
-
-  // 3. Configuración para desactivar reglas que chocan con Prettier (siempre al final)
   eslintConfigPrettier,
-
-  // (Opcional) Puedes añadir tus propias reglas aquí
   {
     rules: {
-      'no-unused-vars': 'warn', // Por ejemplo, cambiar errores de variables no usadas a advertencias
+      // CAMBIO: Cambia 'warn' a 'error' para que el commit falle
+      'no-unused-vars': 'error',
     },
   },
 ];
